04027f7d873189933b4c207b5bbfed26748954ac
Tom Flídr <tomflidr@gmail.com>
2017-10-23 17:29:09 +0200

    VS2017

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -2,34 +2,37 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Databasic.PostgreSql")>
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyDescription("Databasic - C#/VB.NET database utility
+ - focusing on queries primarily based on pure SQL commands, no linq transations
+ - arranging data into primitive types, typed active record classes, collections and more
+ - allowing to run any nonselect queries")>
+<Assembly: AssemblyCompany("Tom Flidr")>
 <Assembly: AssemblyProduct("Databasic.PostgreSql")>
-<Assembly: AssemblyCopyright("Copyright ©  2017")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("Tom Flidr")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("79753dc8-9d0d-4ca6-a6d0-9cfe4bd30938")>
+<Assembly: Guid("e6cfcc1d-9a0a-47c7-a768-f4def499180f")>
 
 ' Version information for an assembly consists of the following four values:
 '
-'   Major Version
-'   Minor Version 
-'   Build Number
-'   Revision
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.2.5.0")>
 <Assembly: AssemblyFileVersion("1.2.5.0")>
